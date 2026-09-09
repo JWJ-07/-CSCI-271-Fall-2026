e916d4fd93f57a6e701f00cbc5e976c3146938bd
JWJ-07 <joshua.williamsjack@jjay.cuny.edu>
2026-09-09 09:12:37 -0400

    hello.cpp
    Fixes output and variable declaration issues.

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     cout << "Hello, world!" << endl;
+     cout << "What is your name?";
+     cin >> name;
+     cout << "Nice to meet you," << name << "!" << end1;
+   
+     return 0;
+
+ }
